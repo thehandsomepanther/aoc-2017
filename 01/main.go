c797b40651dc05d5a5b9d6df27c1f13829dfc55a
Josh Shi <thehandsomepanther@gmail.com>
2017-12-04 14:58:42 -0600

    day 1 part 2 (needs cleanup)

--- 01/main.go
+++ 01/main.go
@@ -19,8 +19,8 @@ func check(e error) {
 	}
 }
 
-func test() bool {
-	dat, err := ioutil.ReadFile("01/tests.json")
+func test1() bool {
+	dat, err := ioutil.ReadFile("01/tests-1.json")
 	check(err)
 	var tests = []testCase{}
 	err = json.Unmarshal(dat, &tests)
@@ -31,7 +31,7 @@ func test() bool {
 	fmt.Println("============")
 	for i := 0; i < len(tests); i++ {
 		fmt.Printf("Test %d: %s ", i+1, tests[i].Input)
-		result := sumCaptcha([]byte(tests[i].Input))
+		result := sumCaptcha1([]byte(tests[i].Input))
 
 		if result == tests[i].Expected {
 			fmt.Printf("✅\n")
@@ -48,23 +48,13 @@ func test() bool {
 	return ok
 }
 
-func sumCaptcha(dat []byte) int {
+func sumCaptcha1(dat []byte) int {
 	captcha := []rune(string(dat))
-
-	var err error
 	total := 0
 
-	firstDigit := captcha[0]
-	finalDigit := captcha[len(captcha)-1]
-
-	if firstDigit == finalDigit {
-		total, err = strconv.Atoi(string(firstDigit))
-		check(err)
-	}
-
 	lastDigit := 0
-	for i := 0; i < len(captcha); i++ {
-		digit, err := strconv.Atoi(string(captcha[i]))
+	for i := 0; i <= len(captcha); i++ {
+		digit, err := strconv.Atoi(string(captcha[i%len(captcha)]))
 		check(err)
 
 		if digit == lastDigit {
@@ -76,9 +66,56 @@ func sumCaptcha(dat []byte) int {
 	return total
 }
 
+func test2() bool {
+	dat, err := ioutil.ReadFile("01/tests-2.json")
+	check(err)
+	var tests = []testCase{}
+	err = json.Unmarshal(dat, &tests)
+	check(err)
+
+	ok := true
+	fmt.Println("Running tests:")
+	fmt.Println("============")
+	for i := 0; i < len(tests); i++ {
+		fmt.Printf("Test %d: %s ", i+1, tests[i].Input)
+		result := sumCaptcha2([]byte(tests[i].Input))
+
+		if result == tests[i].Expected {
+			fmt.Printf("✅\n")
+		} else {
+			fmt.Printf("❌\n")
+			fmt.Printf("Expected: %d\n", tests[i].Expected)
+			fmt.Printf("Got: %d\n", result)
+			ok = false
+		}
+
+		fmt.Println("============")
+	}
+
+	return ok
+}
+
+func sumCaptcha2(dat []byte) int {
+	captcha := []rune(string(dat))
+	total := 0
+
+	for i := 0; i < len(captcha); i++ {
+		digit, err := strconv.Atoi(string(captcha[i%len(captcha)]))
+		check(err)
+		nextDigit, err := strconv.Atoi(string(captcha[(i+len(captcha)/2)%len(captcha)]))
+		check(err)
+
+		if digit == nextDigit {
+			total += digit
+		}
+	}
+
+	return total
+}
+
 func main() {
-	ok := test()
-	// ok := true
+	fmt.Println("Part 1:")
+	ok := test1()
 	if !ok {
 		fmt.Println("Some tests failed and I refuse to run the actual input")
 		os.Exit(3)
@@ -86,5 +123,13 @@ func main() {
 
 	dat, err := ioutil.ReadFile("01/input.txt")
 	check(err)
-	fmt.Printf("Final answer: %d\n", sumCaptcha(dat))
+	fmt.Printf("Final answer for part 1: %d\n", sumCaptcha1(dat))
+
+	fmt.Println("Part 2:")
+	ok = test2()
+	if !ok {
+		fmt.Println("Some tests failed and I refuse to run the actual input")
+		os.Exit(3)
+	}
+	fmt.Printf("Final answer for part 2: %d\n", sumCaptcha2(dat))
 }
