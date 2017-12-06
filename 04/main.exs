a8e1a7ae497ad1e816818c09da4df70712bb1cb3
Josh Shi <thehandsomepanther@gmail.com>
2017-12-05 23:13:12 -0600

    day four part two

--- 04/main.exs
+++ 04/main.exs
@@ -7,23 +7,48 @@ expect = fn (expected, actual) ->
     end
 end
 
-isValidPassphrase = fn phrase -> 
+isUniquePassphrase = fn phrase -> 
     words = String.split(phrase, " ")
     Enum.count(Enum.uniq(words)) === Enum.count(words)
 end
 
-expect.(isValidPassphrase.("aa bb cc dd ee"), true)
-expect.(isValidPassphrase.("aa bb cc dd aa"), false)
-expect.(isValidPassphrase.("aa bb cc dd aaa"), true)
+isNonAnagramicPassphrase = fn phrase ->
+    words = String.split(phrase, " ")
+    Enum.count(words) === Enum.count(Enum.uniq(Enum.map(words, fn word -> 
+        Enum.join(Enum.sort(String.split(word, ""))) 
+    end)))
+end
+
+IO.puts("======== PART 1 ========")
+expect.(isUniquePassphrase.("aa bb cc dd ee"), true)
+expect.(isUniquePassphrase.("aa bb cc dd aa"), false)
+expect.(isUniquePassphrase.("aa bb cc dd aaa"), true)
 
 IO.puts("Running test input")
 { :ok, data } = File.read "04/input.txt"
 lines = String.split(data, "\n")
 answer = Enum.reduce(lines, 0, fn(line, acc) -> 
-    if isValidPassphrase.(line) do 
+    if isUniquePassphrase.(line) do 
         1 + acc 
     else 
         0 + acc 
     end
 end)
-IO.puts("Final answer: #{ answer }")
+IO.puts("Final answer: #{ answer }\n")
+
+IO.puts("======== PART 2 ========")
+expect.(isNonAnagramicPassphrase.("abcde fghij"), true)
+expect.(isNonAnagramicPassphrase.("abcde xyz ecdab"), false)
+expect.(isNonAnagramicPassphrase.("a ab abc abd abf abj"), true)
+expect.(isNonAnagramicPassphrase.("iiii oiii ooii oooi oooo"), true)
+expect.(isNonAnagramicPassphrase.("oiii ioii iioi iiio"), false)
+
+IO.puts("Running test input")
+answer = Enum.reduce(lines, 0, fn(line, acc) -> 
+    if isNonAnagramicPassphrase.(line) do 
+        1 + acc 
+    else 
+        0 + acc 
+    end
+end)
+IO.puts("Final answer: #{ answer }\n")
